033027948360094c1fafbee715fd93d48fabc708
Ron Kurr <kurr@kurron.org>
2015-09-23 13:10:00 +0000

    New JVM arguments

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Use host networking to avoid having to map every hostname needed for the simulation 
-docker run --detach=true --name=rabbitmq-service --net=host --publish=8500:8500 kurron/monitor-rabbitmq:latest 
-docker logs --follow=true --tail=all rabbitmq-service 
+# Use host networking to avoid having to map every hostname needed for the simulation
+docker run --detach=true --name=rabbitmq-service --net=host --publish=8500:8500 kurron/monitor-rabbitmq:latest
+docker logs --follow=true --tail=25 rabbitmq-service
