7bd7ec29c8c19385898deaabe8e97b452f667ad9
Ron Kurr <kurr@kurron.org>
2015-09-23 18:20:27 +0000

    Added JVM monitoring

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Use host networking to avoid having to map every hostname needed for the simulation
-docker run --detach=true --name=rabbitmq-service --net=host --publish=8500:8500 kurron/monitor-rabbitmq:latest
+docker run --detach=true --name=rabbitmq-service --net=host --publish=8500:8500 --env=NEW_RELIC_APP_NAME=rabbitmq-service --env=NEW_RELIC_HOST_DISPLAY_NAME=${HOSTNAME} kurron/monitor-rabbitmq:latest
 docker logs --follow=true --tail=25 rabbitmq-service
